d0bde71e20787b7fde95df70a1cbbe4421d43639
sanlad17 <santiagoladino286@gmail.com>
2025-11-09 12:50:59 -0500

    Delete promedio aritmetico.py

--- promedio aritmetico.py
+++
@@ -1,9 +0,0 @@
-def promedio_aritmetico():
-    n1 = float(input('Ingrese el primer numero: '))
-    n2 = float(input('Ingrese el segundo numero: '))
-    n3 = float(input('Ingrese el tercer numero: '))
-    promedio = (n1 + n2 + n3) / 3
-    print(f'El promedio de sus numeros es de {promedio}')
-
-# Ejecutar función
-promedio_aritmetico()
