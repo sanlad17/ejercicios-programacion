afe816cb6c0c9b008e14b3f3f10f8e3bdb7409e2
sanlad17 <santiagoladino286@gmail.com>
2025-11-09 12:51:12 -0500

    Delete operacion modulo.py

--- operacion modulo.py
+++
@@ -1,9 +0,0 @@
-def operacion_modulo():
-    numero2 = int(input("Ingrese el segundo número: "))
-    
-    resultado = numero1 % numero2
-    
-    print(f"El resultado de {numero1} MOD {numero2} es: {resultado}")
-
-# Ejecutar función
-operacion_modulo()
