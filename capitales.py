ecd641f81d7734bb20d2167650eebb56697f301e
sanlad17 <santiagoladino286@gmail.com>
2025-11-09 12:44:45 -0500

    Delete capitales.py

--- capitales.py
+++
@@ -1,7 +0,0 @@
-def capitales():
-    pais = input('Ingrese el nombre del pais: ')
-    capital = input('Ingrese su capital: ')
-    print(f'{capital} es la capital de {pais}')
-
-# Ejecutar función
-capitales()
