bc4da58eede534dfe7f0677b879e3d1265a8b692
sanlad17 <santiagoladino286@gmail.com>
2025-11-09 12:47:45 -0500

    Delete doble y triple.py

--- doble y triple.py
+++
@@ -1,9 +0,0 @@
-def doble_y_triple():
-    n1 = float(input('Ingrese el valor del numero: '))
-    do = n1 * 2
-    print(f'El doble de su numero es {do}')
-    tri = n1 * 3
-    print(f'El triple de su numero es {tri}')
-
-# Ejecutar función
-doble_y_triple()
