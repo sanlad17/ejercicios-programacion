fb985258d6c9028b8e0d87d41cf9ff2b4a1dfd74
sanlad17 <santiagoladino286@gmail.com>
2025-11-09 12:50:35 -0500

    Delete temperatura.py

--- temperatura.py
+++
@@ -1,7 +0,0 @@
-def temperatura():
-    grados_celsius = float(input('Ingrese la temperatura en grados celsius: '))
-    fahrenheit = 9/5 * grados_celsius + 32
-    print(f'Su temperatura es de {fahrenheit}')
-
-# Ejecutar función
-temperatura()
