421b67c1fb14c022f39fdd5d16670a13722ca855
sanlad17 <santiagoladino286@gmail.com>
2025-11-09 12:50:27 -0500

    Delete valor siguiente.py

--- valor siguiente.py
+++
@@ -1,7 +0,0 @@
-def valor_siguiente():
-    num = float(input('Ingrese su numero: '))
-    siguiente = num + 1
-    print(f'El siguiente numero del numero ingresado es {siguiente}')
-
-# Ejecutar función
-valor_siguiente()
